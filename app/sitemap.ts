85a9b9d6eb48b255e168a642ee4e934f2aef0bab
Firat Kaya <firatkaya8234@gmail.com>
2026-05-31 01:29:43 +0300

    Add sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,28 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://frtky.com";
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/about`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/projects`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/blog`,
+      lastModified: new Date(),
+    },
+    {
+      url: `${baseUrl}/contact`,
+      lastModified: new Date(),
+    },
+  ];
+}
